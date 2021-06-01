1d2de2e123d585fdb13eb06aaf1d051830bc11da
Nico Proto <nicolasproto@gmail.com>
2021-06-01 17:42:32 +0200

    Added reviews and associations, validations for all models

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,10 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
+
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings
+
+  validates :name, :location, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, length: { minimum: 25 }
 end
