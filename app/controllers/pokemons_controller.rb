6ae6507294f555aa6e457e2f1c2dac4ead6866f9
Nico Proto <nicolasproto@gmail.com>
2021-06-01 17:54:24 +0200

    Added nickname to devise views, started setup for act_as_taggable gem

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,4 +4,25 @@ class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all
   end
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    @pokemon.user = current_user
+
+    if @pokemon.save
+      redirect_to pokemon_path(@pokemon)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :description, :price, :location)
+  end
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,8 @@
 class Pokemon < ApplicationRecord
+  CATEGORIES = %w[electric fire water grass flying poison bug normal ground].freeze
+
+  acts_as_taggable_on :tags # or whatever you would like to call your tag list.
+
   belongs_to :user
 
   has_many :bookings, dependent: :destroy
