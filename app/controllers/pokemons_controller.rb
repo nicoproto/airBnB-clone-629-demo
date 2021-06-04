e3eebe68991d7e9d65cd2df1fb8fea9da3fec0d0
Nico Proto <nicolasproto@gmail.com>
2021-06-04 14:25:00 +0200

    Allowed show without login

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,5 +1,5 @@
 class PokemonsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @pokemons = Pokemon.all
